68e33d264b4c9a65ad296036988d48c5910d1fb2
securedigital0 <47745753+securedigital0@users.noreply.github.com>
2019-02-21 21:33:54 +0800

    Added Character Class
    The Character Class (version one, lol) allows for quick and flexible creation of Characters and Sprites, along with easier texture setting and movement options.

---
+++ Puzzle Dungeon/character.hpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string.h>
+#include <SFML/Graphics.hpp>
+
+class Character { 
+public:
+	void setPosition(int x, int y);
+	void setTexture(std::string MyTexture);
+	void move(char key);
+	sf::Sprite mySprite;
+private:
+	sf::Texture texture;
+};
+
+void Character::setPosition(int x, int y) {
+	mySprite.setPosition(x, y);
+}
+
+void Character::setTexture(std::string MyTexture) {
+	texture.loadFromFile(MyTexture);
+	mySprite.setTexture(texture);
+}
+
+
+void Character::move(char key) {
+	sf::Vector2f movement(0.f, 0.f);
+	switch(key) {
+		case 'A':
+			mySprite.move(-5.f, 0.f);
+			break;
+		case 'W':
+			mySprite.move(0.f, -5.f);
+			break;
+		case 'S':
+			mySprite.move(0.f, 5.f);
+			break;
+		case 'D':
+			mySprite.move(5.f, 0.f);
+			break;
+	}
+
+}
+
+
+/*
+
+	GameInstance class checks for input, switch case (A, W, D, S)
+	Tells Character class which one was pressed in a char value, 
+	Character moves according to letter
+
+	*/
