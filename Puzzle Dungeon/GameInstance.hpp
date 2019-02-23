21915b30fd4a8d37329375c88c3c924013f3c8fc
UltraFuture7000 <omarjunaid22@gmail.com>
2019-02-23 15:43:37 +0800

    Fixed license format

--- Puzzle Dungeon/GameInstance.hpp
+++ Puzzle Dungeon/GameInstance.hpp
@@ -1,9 +1,6 @@
 /*
 Puzzle Dungeon ((C) 2019)
 @authors Omar Junaid, Sam Dockery
-/*
-Puzzle Dungeon ((C) 2019)
-@authors Omar Junaid, Sam Dockery
 This work is licensed under the
 Creative Commons Attribution-NonCommercial-NoDerivatives
 4.0 International License. To view a copy of this license,
