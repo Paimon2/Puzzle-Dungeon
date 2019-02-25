6322d2e5a54b92b0876a78bd0774fe09a939cccd
UltraFuture7000 <omarjunaid22@gmail.com>
2019-02-25 20:41:20 +0800

    Added areSpritesColliding() in Helpers.hpp

---
+++ Puzzle Dungeon/Helpers.hpp
@@ -0,0 +1,25 @@
+/*
+Puzzle Dungeon ((C) 2019)
+@authors Omar Junaid, Sam Dockery
+This work is licensed under the
+Creative Commons Attribution-NonCommercial-NoDerivatives
+4.0 International License. To view a copy of this license,
+visit http://creativecommons.org/licenses/by-nc-nd/4.0/.
+*/
+
+#ifndef _HELPERS_HPP_
+#define _HELPERS_HPP_
+
+#include <vector>
+#include "SFML/Graphics.hpp"
+
+namespace Helpers {
+
+	bool areSpritesColliding(sf::Sprite &sprite1, sf::Sprite &sprite2) {
+		return (sprite1.getGlobalBounds().intersects(sprite2.getGlobalBounds()));
+	}
+
+}
+
+
+#endif
